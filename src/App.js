4aa1410ec2ce2f966d19b5b3454af3935bcbfb46
Andrew Nderitu <49023221+andymush@users.noreply.github.com>
2020-12-16 21:32:24 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
 
+import axios from 'axios';
+import './App.css';
+import React, { useEffect,useState } from 'react';
+import Button from 'react-bootstrap/Button'
+import { ButtonToolbar } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
+  const [state,setState] =useState({
+    joke:''
+  })
+
+  useEffect( () => {
+    fetchData();
+  },[]);
+
+const fetchData =async()=> {
+  const result = axios.get('https://api.chucknorris.io/jokes/random');
+  console.log((await result).data.value);
+  setState({
+    ...state,
+    joke: (await result).data.value
+  })
+}
+
+function refreshPage() {
+  window.location.reload(false);
+}    
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <h1 className="title">Chuck Norris Joke</h1>
+          <br></br>
+        </div>
+      </div>
+          <ButtonToolbar>
+            <button className="btn btn-primary" onClick={refreshPage}>Get a random Joke</button>
+          </ButtonToolbar>
+        <br>
+        </br>
+      <h4>{state.joke}</h4>
+      
+      </div>
   );
 }
+
 
 export default App;
